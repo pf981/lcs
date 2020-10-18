1c9d15b12055f3241b051740cc53ba18f0d975b2
Paul Foster <paul.foster@vgw.co>
2020-10-18 17:12:56 +1100

    Remove extra braces

--- src/lcs.cpp
+++ src/lcs.cpp
@@ -37,9 +37,8 @@ String longest_common_substring_single(String str1, String str2) {
 
 // [[Rcpp::export]]
 StringVector longest_common_substring(StringVector str1, StringVector str2) {
-  if (str1.length() != str2.length()) {
+  if (str1.length() != str2.length())
     stop("str1 and str2 must be the same length.");
-  }
   
   return mapply(str1, str2, longest_common_substring_single);
 }
