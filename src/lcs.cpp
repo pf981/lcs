ff34913d2b9ac5bd0a7a6805c55e4e1c15ab8345
Paul Foster <paul.foster@vgw.co>
2020-10-18 19:21:53 +1100

    WIP: Add Hunt-Szymanski Algorithm

--- src/lcs.cpp
+++ src/lcs.cpp
@@ -35,48 +35,133 @@ String longest_common_substring_single(String str1, String str2) {
   return result;
 }
 
+// String longest_common_subsequence_single(String str1, String str2) {
+//   std::string a = str1;
+//   std::string b = str2;
+//   int r = a.length();
+//   int n = b.length();
+//   std::vector<std::vector<int> > table(r + 1, std::vector<int>(n + 1));
+//   
+//   for (int i = 0; i < r; i++)
+//     table[i][0] = 0;
+//   
+//   for (int j = 0; j < n; j++)
+//     table[0][j] = 0;
+//   
+//   for (int i = 1; i <= r; i++) {
+//     for (int j = 1; j <= n; j++) {
+//       if (a[i - 1] == b[j - 1])
+//         table[i][j] = table[i - 1][j - 1] + 1;
+//       else
+//         table[i][j] = std::max(table[i][j - 1], table[i - 1][j]);
+//     }
+//   }
+//   
+//   int index = table[r][n];
+//   std::string result(index, ' ');
+//   int i = r;
+//   int j = n;
+//   while (i > 0 && j > 0) { 
+//     if (a[i - 1] == b[j - 1]) { 
+//       result[index - 1] = a[i - 1];
+//       --i;
+//       --j;
+//       --index;
+//     }
+//     else if (table[i - 1][j] > table[i][j - 1]) {
+//       --i; 
+//     }
+//     else {
+//       --j;
+//     }
+//   }
+//   
+//   return result;
+// }
+
+// Hunt-Szymanski Algorithm
 String longest_common_subsequence_single(String str1, String str2) {
   std::string a = str1;
   std::string b = str2;
   int r = a.length();
   int n = b.length();
   std::vector<std::vector<int> > table(r + 1, std::vector<int>(n + 1));
-  
-  for (int i = 0; i < r; i++)
-    table[i][0] = 0;
-  
-  for (int j = 0; j < n; j++)
-    table[0][j] = 0;
-  
-  for (int i = 1; i <= r; i++) {
-    for (int j = 1; j <= n; j++) {
-      if (a[i - 1] == b[j - 1])
-        table[i][j] = table[i - 1][j - 1] + 1;
-      else
-        table[i][j] = std::max(table[i][j - 1], table[i - 1][j]);
-    }
-  }
-  
+
+
   int index = table[r][n];
   std::string result(index, ' ');
-  int i = r;
-  int j = n;
-  while (i > 0 && j > 0) { 
-    if (a[i - 1] == b[j - 1]) { 
-      result[index - 1] = a[i - 1];
-      i--;
-      j--;
-      index--;
-    }
-    else if (table[i - 1][j] > table[i][j - 1]) {
-      i--; 
-    }
-    else {
-      j--;
+
+
+
+
+  int i, j, k, LCS, high, low,mid, alphabet_size = 256;
+  int *matchlist[alphabet_size], *L;
+  for (i = 0; i < alphabet_size; i++){
+    matchlist[i] = (int*)calloc((n + 2), sizeof(int));
+  }
+  L = (int*)calloc((n + 1), sizeof(int));
+
+  /*-----make the matchlist-----*/
+  for (i = 1; i <= m; i++){
+    if (matchlist[stringA[i]][0] == 0){
+      matchlist[stringA[i]][0] = 1;
+
+      for (k = 1,j = n; j>0; j--){
+        if (stringA[i] == stringB[j]){
+          matchlist[stringA[i]][k] = j;
+          k++;
+        }
+        matchlist[stringA[i]][k] = -1;
+      }
     }
   }
-  
-  return result;
+
+
+  /*-----finding the LCS-----*/
+  for (LCS = 0, i = 1; i <= m; i++){
+    for (j = 1; matchlist[stringA[i]][j] != -1; j++){
+
+      /*-----if the number bigger then the biggest number in the L, LCS + 1-----*/
+      if (matchlist[stringA[i]][j] > L[LCS]){
+        LCS++;
+        L[LCS] = matchlist[stringA[i]][j];
+      }
+
+      /*-----else, do the binary search to find the place to insert the number-----*/
+      else{
+        high = LCS;
+        low = 0;
+        k = 0;
+        while (1){
+          mid = low + ((high - low) / 2);
+          if (L[mid] == matchlist[stringA[i]][j]){
+            k = 1;
+            break;
+          }
+          if (high - low <= 1){
+            mid = high;
+            break;
+          }
+          if (L[mid] > matchlist[stringA[i]][j]){
+            high = mid;
+          }
+
+          else if (L[mid] < matchlist[stringA[i]][j]){
+            low = mid;
+          }
+        }
+        if (k == 0){
+          L[mid] = matchlist[stringA[i]][j];
+        }
+      }
+    }
+  }
+  for (i = 0; i < 256; i++){
+    free(matchlist[i]);
+  }
+  free(matchlist);
+  free(L);
+  return LCS;
 }
 
 //' Longest common substring
